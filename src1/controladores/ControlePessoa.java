b2cf060bd64fe65a9845ccd7974832fe68ac3ec8
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2019-12-16 01:46:37 -0300

    Update ControlePessoa.java

--- src1/controladores/ControlePessoa.java
+++ src1/controladores/ControlePessoa.java
@@ -10,10 +10,10 @@ import entidades.Validacao;
 /**
  * Representação de um Controle de Pessoa, responsável por cadastrar uma pessoa, deputado ou listar pessoa.
  * 
- * @author Andre Luis Souza de Andrade Santana - Matricula: 118210116
- * @author Caio Jose dos Santos Arruda - Matricula: 118210844
- * @author Joao Vitor Romao Patricio - Matricula: 118211058
- * @author Sheila Maria Mendes Paiva - Matricula: 118210186
+ * @author Andre Luis Souza de Andrade Santana 
+ * @author Caio Jose dos Santos Arruda 
+ * @author Joao Vitor Romao Patricio 
+ * @author Sheila Maria Mendes Paiva 
  */
 public class ControlePessoa implements Serializable {
 	
