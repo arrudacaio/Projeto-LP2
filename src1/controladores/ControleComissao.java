b5bc44d96db30fdc77629ac08e68863c029638d1
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2019-12-16 01:46:07 -0300

    Update ControleComissao.java

--- src1/controladores/ControleComissao.java
+++ src1/controladores/ControleComissao.java
@@ -9,10 +9,10 @@ import entidades.Comissao;
 /**
  * Representacao o Controle de uma Comissao.
  * 
- * @author Andre Luis Souza de Andrade Santana - Matricula: 118210116
- * @author Caio Jose dos Santos Arruda - Matricula: 118210844
- * @author Joao Vitor Romao Patricio - Matricula: 118211058
- * @author Sheila Maria Mendes Paiva - Matricula: 118210186
+ * @author Andre Luis Souza de Andrade Santana 
+ * @author Caio Jose dos Santos Arruda 
+ * @author Joao Vitor Romao Patricio 
+ * @author Sheila Maria Mendes Paiva 
  */
 public class ControleComissao implements Serializable {
 	
